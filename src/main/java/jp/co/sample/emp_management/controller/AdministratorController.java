7c00c672d33a0fda95bbc9fd05dea38146994f1a
ra-mori <shigeki.morishita@rakus.co.jp>
2021-06-09 09:14:01 +0900

    FieldErrorの実装とHTMLの改修

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -83,7 +84,8 @@ public class AdministratorController {
 			administratorService.insert(administrator);
 			return "redirect:/";
 		}
-		model.addAttribute("different", "パスワードと確認用パスワードが一致しません");
+		FieldError error = new FieldError("different", "password", "パスワードと確認用パスワードが一致しません");
+		result.addError(error);
 		return toInsert(model);
 	}
 
